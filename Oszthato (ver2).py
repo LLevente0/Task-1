34aade845b0f91daec41c848cf605f25929e3926
Ladányi Levente <ladanyi.levente@diak.szbi-pg.hu>
2025-02-12 14:12:52 +0100

    Update Oszthato (ver2).py

--- Oszthato (ver2).py
+++ Oszthato (ver2).py
@@ -4,3 +4,19 @@ ami egy egész számot kap paraméterként és igaz értéket ad vissza, ha a sz
 A függvény segítségével számolja ki azon 3 jegyű számok átlagát, amire a függvény igaz értékkel tér vissza.
 Minta:
 A 7-tel osztható és 3-mal nem osztható 3 jegyű számok átlaga: 551.2705882352941"""
+
+haromjegyu_szamok = []
+
+for szam in range(100, 1000):
+    if szam % 7 == 0 and szam % 3 != 0:
+        haromjegyu_szamok.append(szam)
+
+# print(haromjegyu_szamok)
+
+def oszthato(szamok):
+    osszeg = sum(szamok)
+    darab_szamok = len(szamok)
+    atlag = osszeg / darab_szamok
+    return atlag
+
+print(oszthato(haromjegyu_szamok))
