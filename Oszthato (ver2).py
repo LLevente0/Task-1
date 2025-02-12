9bf0fb3158aa52d3c85a499ff2d4285ae00c5124
Ladányi Levente <ladanyi.levente@diak.szbi-pg.hu>
2025-02-12 14:05:25 +0100

    Create Oszthato (ver2).py

---
+++ Oszthato (ver2).py
@@ -0,0 +1,6 @@
+"""2. feladat: Oszthatóság	14 pont
+Írjon programot oszthato.py néven. A programban hozzon létre egy függvényt oszthato néven,
+ami egy egész számot kap paraméterként és igaz értéket ad vissza, ha a szám 7-tel osztható, de 3-mal nem.
+A függvény segítségével számolja ki azon 3 jegyű számok átlagát, amire a függvény igaz értékkel tér vissza.
+Minta:
+A 7-tel osztható és 3-mal nem osztható 3 jegyű számok átlaga: 551.2705882352941"""
